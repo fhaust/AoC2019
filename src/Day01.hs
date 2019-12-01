1bc69351ebb1aff0cf303a88249e61814b6c9c86
Florian Hofmann <fho@f12n.de>
2019-12-01 19:57:16 +0100

    alternate version of day 1b

--- src/Day01.hs
+++ src/Day01.hs
@@ -2,6 +2,11 @@
 requiredFuel1 x = x `div` 3 - 2
 requiredFuel2 = sum . takeWhile (>0) . drop 1 . iterate (max 0 . requiredFuel1)
 
+-- probably more readable than the equivalent requiredFuel2
+requiredFuel3 x | x' > 0     = x' + requiredFuel3 x'
+                | otherwise  = x'
+  where x' = max 0 (requiredFuel1 x)
+
 main = do
 
   input <- map read . lines <$> readFile "inputs/day01.txt" :: IO [Int]
